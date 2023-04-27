20c9d2fa4224637078e59266eaceab9706d63533
Gautam Raj <51695470+gautamraj873@users.noreply.github.com>
2023-04-28 04:37:46 +0530

    returns responses based on urls

--- first_app.js
+++ first_app.js
@@ -1,7 +1,30 @@
-const http = require('http');
+const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('Gautam');
-});
+    if(req.url === '/home'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Home Page</title></head>');
+        res.write('<body><h1>Welcome home</h2></body>');
+        res.write('</html>');
+        res.end();
+    }
+    else if (req.url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>About Us</title></head>');
+        res.write('<body><h1>Welcome to About Us page!</h1></body>');
+        res.write('</html>');
+        res.end();
+    } 
+    else if (req.url === '/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Node Js project</title></head>');
+        res.write('<body><h1>Welcome to my Node Js project!</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+})
 
-server.listen(4000);
+server.listen(3000);
