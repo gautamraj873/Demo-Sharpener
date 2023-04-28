6eacab2813dffbe5cfd2e31368552b10befd6b16
Gautam Raj <51695470+gautamraj873@users.noreply.github.com>
2023-04-29 02:25:31 +0530

    message showing at the top of the form

--- first_app.js
+++ first_app.js
@@ -1,30 +1,50 @@
 const http = require('http')
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    if(req.url === '/home'){
-        res.setHeader('Content-Type', 'text/html');
+const url = req.url;
+const method = req.method;
+
+if(url === '/'){
+    fs.readFile('message.txt', {encoding: "utf-8"}, (err,data) => {
+        if(err){
+            console.log(err);
+        }
         res.write('<html>');
-        res.write('<head><title>Home Page</title></head>');
-        res.write('<body><h1>Welcome home</h2></body>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write(`<body>${data}</body>`);
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        res.end();
-    }
-    else if (req.url === '/about'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>About Us</title></head>');
-        res.write('<body><h1>Welcome to About Us page!</h1></body>');
-        res.write('</html>');
-        res.end();
-    } 
-    else if (req.url === '/node'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>Node Js project</title></head>');
-        res.write('<body><h1>Welcome to my Node Js project!</h1></body>');
-        res.write('</html>');
-        res.end();
-    }
+        return res.end();
+    })
+}
+
+else if(url === '/message' && method === 'POST'){
+    const body = [];
+    req.on('data', (chunk) => {
+        body.push(chunk);
+    });
+    req.on('end',() => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split('=')[1];
+        fs.writeFileSync('message.txt', message, (err) => {
+            if(err){
+                console.log(err);
+            }
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+    });
+}
+else{
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>Node Js project</title></head>');
+    res.write('<body><h1>Hello from Node Js Server!</h1></body>');
+    res.write('</html>');
+    res.end();
+}
 })
 
 server.listen(3000);
