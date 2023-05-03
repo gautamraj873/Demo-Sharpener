414c8cb61b17b677231b8373a740d4a8f97738b2
Gautam Raj <51695470+gautamraj873@users.noreply.github.com>
2023-05-04 03:20:41 +0530

    one more input tag added for size

--- first_app.js
+++ first_app.js
@@ -1,14 +1,20 @@
-const express = require('express'); 
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(function (req, res, next){
-    console.log('In the middleware');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', function(req, res, next){
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use(function (req, res, next){
-    console.log('In another middleware');
+app.post('/product', function(req, res, next){
+    console.log(req.body);
+    res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello from Express!</h1>');
 });
 
